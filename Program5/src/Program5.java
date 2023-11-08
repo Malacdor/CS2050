9cde2160977594ba05df1687d72850e21aa05028
Diego Salas <142828587+Malacdor@users.noreply.github.com>
2023-11-08 01:16:55 -0700

    Add files via upload

---
+++ Program5/src/Program5.java
@@ -0,0 +1,141 @@
+//Diego Salas
+//cs 2050
+import java.io.*;
+import java.util.Scanner;
+import java.util.Stack;
+
+
+public class Program5
+{
+    public static void main(String[] args)
+    {
+        try
+        {
+            Scanner inputFile = new Scanner(new File("Program3.txt"));
+            PrintWriter outputFile = new PrintWriter(new FileWriter("Program5.out"));
+
+            Stack<Character>Stack =new Stack<>();
+
+            while (inputFile.hasNextLine())
+            {
+                String infixExpression = inputFile.nextLine();
+                String postfixExpression = infixToPostfix(infixExpression);
+                if (postfixExpression != null)
+                {
+                    outputFile.println(infixExpression + " -> " + postfixExpression);
+                } else
+                {
+                    // Handle unmatched parentheses and invalid characters
+                    outputFile.println(infixExpression + " -> Error: Unmatched Parens or Invalid Character");
+                }
+            }
+
+            inputFile.close();
+            outputFile.close();
+        } catch (FileNotFoundException e)
+        {
+            System.out.println("File not found: " + e.getMessage());
+        } catch (IOException e) {
+            System.out.println("Error writing to the output file: " + e.getMessage());
+        }
+    }
+
+    public static String infixToPostfix(String s)
+    {
+        StringBuilder postfix = new StringBuilder();
+        Stack<Character> stack = new Stack<>();
+
+        boolean hasInvalidCharacter = false;
+
+        for (char c : s.toCharArray())
+        {
+            if (isValidCharacter(c))
+            {
+                if (isOperand(c))
+                {
+                    postfix.append(c);
+                    postfix.append(' ');
+                } else if (c == '(')
+                {
+                    stack.push(c);
+                } else if (c == ')')
+                {
+                    while (!stack.isEmpty() && stack.peek() != '(')
+                    {
+                        postfix.append(stack.pop());
+                        postfix.append(' ');
+                    }
+                    if (!stack.isEmpty() && stack.peek() == '(')
+                    {
+                        stack.pop();
+                    } else {
+                        // Unmatched right parenthesis
+                        return null;
+                    }
+                } else if (isOperator(c))
+                {
+                    while (!stack.isEmpty() && precedence(c) <= precedence(stack.peek()))
+                    {
+                        postfix.append(stack.pop());
+                        postfix.append(' ');
+                    }
+                    stack.push(c);
+                }
+            } else
+            {
+                // Invalid character
+                hasInvalidCharacter = true;
+            }
+        }
+
+        while (!stack.isEmpty())
+        {
+            if (stack.peek() == '(')
+            {
+                // Unmatched left parenthesis
+                return null;
+            }
+            postfix.append(stack.pop());
+            postfix.append(' ');
+        }
+
+        if (hasInvalidCharacter)
+        {
+            return null;
+        }
+
+        return postfix.toString().trim();
+    }
+
+    private static boolean isValidCharacter(char c)
+    {
+        // Only allow digits, decimal point, parentheses, and the specified operators
+        return Character.isDigit(c) || c == '.' || c == '(' || c == ')' || isOperator(c) || c == ' ';
+    }
+
+    private static boolean isOperand(char c)
+    {
+        // Allow digits and decimal point
+        return Character.isDigit(c) || c == '.';
+    }
+
+    private static boolean isOperator(char c)
+    {
+        // Only allow these operators
+        return c == '+' || c == '-' || c == '*' || c == '/' || c == '%' || c == '(' || c == ')';
+    }
+
+    private static int precedence(char c)
+    {
+        if (c == '+' || c == '-') {
+            return 1;
+        } else if (c == '*' || c == '/' || c == '%')
+        {
+            return 2;
+        } else if (c == '(' || c == ')')
+        {
+            return 0; // Parentheses should not affect precedence
+        }
+        return -1; // Default precedence for other characters
+    }
+}
